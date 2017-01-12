7b30a7f902b21baf2731fd30ca43494cb681f3df
yujiasabrinaxie <sabrinaxie001@gmail.com>
2017-01-12 01:20:21 -0800

    added gitignore and updated spider

--- movie/pipelines.py
+++ movie/pipelines.py
@@ -6,26 +6,54 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Index
 from elasticsearch_dsl.document import DocType
-from elasticsearch_dsl.mapping import Mapping
-from elasticsearch_dsl.field import String, Integer, Date
+from elasticsearch_dsl.field import Integer, Date, Float, Keyword, Text
 
 es = Elasticsearch()
 
+
 class Movie(DocType):
-    title = String()
+    title = Text(fields={'raw': {'type': 'keyword'}})
     vote = Integer()
-    creators = String(multi=True)
+    creators = Keyword(multi=True)
+    genre = Keyword(multi=True)
+    cast = Keyword(multi=True)
+    time = Integer()
+    country = Keyword(multi=True)
+    plot_keywords = Keyword(multi=True)
+    language = Keyword(multi=True)
+    filming_locations = Keyword(multi=True)
+    release_date = Date()
+    ratingvalue = Float()
+    poster = Keyword()
+    summary = Text()
 
     class Meta:
         index = 'imdb'
 
-Movie.delete(using=es)
-Movie.init(using=es)
+
 
 class MoviePipeline(object):
+    def __init__(self):
+        movies = Index('imdb', using=es)
+        movies.doc_type(Movie)
+        movies.delete(ignore=404)
+        movies.create()
+
     def process_item(self, item, spider):
         movie = Movie(title=item['title'], vote=item['vote'])
         movie.creators = item['creators']
+        movie.genre = item['genre']
+        movie.cast = item['cast']
+        movie.time = item['time']
+        movie.country = item['country']
+        movie.plot_keywords = item['plot_keywords']
+        movie.language = item['language']
+        movie.filming_locations = item['filming_locations']
+        movie.release_date = item['release_date']
+        movie.ratingvalue = item['ratingvalue']
+        movie.poster = item['poster']
+        movie.summary = item['summary']
         movie.save(using=es)
         return item
--- movie/spiders/MovieSpider.py
+++ movie/spiders/MovieSpider.py
@@ -1,14 +1,36 @@
-
 import scrapy
-import json
-import time
 import re
+import logging
+import sys
+import time
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+month_mapping = {'January': '01',
+                 'February': '02',
+                 'March': '03',
+                 'April': '04',
+                 'May': '05',
+                 'June': '06',
+                 'July': '07',
+                 'August': '08',
+                 'September': '09',
+                 'October': '10',
+                 'November': '11',
+                 'December': '12'}
 
 
 class moviespider(scrapy.Spider):
     name = "imdb_spider"
     start_urls = [
-        """http://www.imdb.com/search/title?release_date=2010-01-01,2016-12-31&user_rating=8.0,10"""
+        """http://www.imdb.com/search/title?release_date=1980-01-01,2018-01-01&title_type=feature&user_rating=7.0,10"""
     ]
     imdbhome = 'http://www.imdb.com'
     cur_page = 1
@@ -22,10 +44,14 @@ class moviespider(scrapy.Spider):
         end = re.search('\d ', total_matching)
         total_matching = int(filter(lambda x: x.isdigit(), total_matching[begin.start() + 1: end.start() + 1]))
         numpages = total_matching / 50
-        while self.cur_page < numpages:
-            for href in movie_hrefs:
-                yield scrapy.Request(href, callback=self.parseMovie)
-            self.cur_page += 1
+        logger.debug('totally %d pages and current scraping page %d', numpages, self.cur_page)
+
+        logger.debug('Number of movies page %d is %d', self.cur_page, len(movie_hrefs))
+        for href in movie_hrefs:
+            yield scrapy.Request(href, callback=self.parseMovie)
+            time.sleep(1)
+        self.cur_page += 1
+        if self.cur_page < numpages:
             yield scrapy.Request(self.get_page_url(self.cur_page), callback=self.parse)
 
     def parseMovie(self, response):
@@ -34,38 +60,55 @@ class moviespider(scrapy.Spider):
         title = response.xpath("//h1[@itemprop='name']/text()").extract_first()
         vote = response.xpath("//span[@itemprop='ratingCount']/text()").extract()[-1]
         genre = response.xpath("//span[@itemprop='genre']/text()").extract()
-        creators =response.xpath("//span[@itemprop='creator']//span[@itemprop='name']/text()").extract()
+        creators = response.xpath("//span[@itemprop='creator']//span[@itemprop='name']/text()").extract()
         cast = response.xpath("//td[@itemprop='actor']//span[@itemprop='name']/text()").extract()
-        #first_two_cast = response.xpath("//td[@itemprop='actor']//span[@itemprop='name']/text()").extract()[:2]
         time = response.xpath("//time[@datetime]/text()").extract()[-1]
-        plot_keywords = response.xpath("//div[@itemprop='keywords']//span[@itemprop='keywords']/text").extract()
-        country = response.xpath("//h4[@class='inline']//a[@itemprop='url']/text()").extract()
-        language = response.xpath("h4[contains(text(),'Language')]/..text()").extract()
-        filming_locations = response.xpath("h4[contains(text(),'Filming Locations')]/..text()").extract()
-        release_date = response.xpath("h4[contains(text(),'Release Date:')]/../text()").extract()
+        plot_keywords = response.xpath("//div[@itemprop='keywords']//span[@itemprop='keywords']/text()").extract()
+        ratingvalue = response.xpath("//div[@class='ratingValue']//span[@itemprop='ratingValue']/text()").extract()[0]
+        release_date = response.xpath("//h4[text()='Release Date:']/../text()").extract()[1]
+        country = response.xpath("//div[h4[text() = 'Country:']]/a/text()").extract()
+        language = response.xpath("//div[h4[text() = 'Language:']]/a/text()").extract()
+        filming_locations = response.xpath("//div[h4[text() = 'Filming Locations:']]/a/text()").extract()
+        poster = response.xpath('//div[@class="poster"]//img/@src').extract()[0]
+        summary = response.xpath('//div[@class="summary_text"]/text()').extract()[0]
 
         yield {'genre': self.normalize_list(genre),
                'vote': self.normalize_int(self.normalize_string(vote)),
                'title': self.normalize_string(title),
-               'creators':self.normalize_list(creators),
-               'cast':self.normalize_list(cast),
+               'creators': self.normalize_list(creators),
+               'cast': self.normalize_list(cast),
                'time': self.normalize_int(self.normalize_string(time)),
-               'country': self.normalize_string(country),
-               'plot_keywords': self.normalize_string(plot_keywords),
-               'language': self.normalize_string(language),
+               'country': self.normalize_list(country),
+               'plot_keywords': self.normalize_list(plot_keywords),
+               'language': self.normalize_list(language),
                'filming_locations': self.normalize_list(filming_locations),
-               'release_date': self.normalize_string(release_date),
+               'release_date': self.normalize_date(release_date),
+               'ratingvalue': self.normalize_float(ratingvalue),
+               'poster': poster,
+               'summary': self.normalize_string(summary)
                }
 
-
     def get_page_url(self, page):
-        return "http://www.imdb.com/search/title?release_date=2010-01-01,2016-12-31&user_rating=8.0,10&page=%d" % page
+            return """http://www.imdb.com/search/title?release_date=1980-01-01,2018-01-01&title_type=feature&user_rating=7.0,10&page=%d""" % page
 
     def normalize_string(self, s):
-        return str(filter(lambda x: ord(x) < 128, s)).rstrip()
+        return str(filter(lambda x: ord(x) < 128, s)).rstrip().lstrip()
 
     def normalize_list(self, l):
         return [self.normalize_string(x) for x in l]
 
     def normalize_int(self, s):
         return int(filter(lambda x: x.isdigit(), s))
+
+    def normalize_date(self, s):
+        date = self.normalize_string(s)
+        splited_date = date.split(' ')
+        splited_date[1] = month_mapping[splited_date[1]]
+        if int(splited_date[0]) < 10:
+            splited_date[0] = '0' + splited_date[0]
+        print 'splited date is %s' % splited_date
+        print 'reversed splited date is %s' % splited_date[:3][::-1]
+        return '-'.join(splited_date[:3][::-1])  # reverse
+
+    def normalize_float(self, s):
+        return float(filter(lambda x: x.isdigit() or x == '.', s))
